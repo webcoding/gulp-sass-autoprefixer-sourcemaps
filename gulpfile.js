9df66a4d7ffa7daad92aa3315a3e2ad9d66128f9
Kendrick Arnett <kmarnett@gmail.com>
2015-07-16 16:41:38 -0500

    Initial build skeleton.

---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-ruby-sass');
+var sourcemaps  = require('gulp-sourcemaps');
+var prefix      = require('gulp-autoprefixer');
+var minifyCss   = require('gulp-minify-css');
+var rename      = require('gulp-rename');
+var browserSync = require('browser-sync').create();
+var scsslint = require('gulp-scss-lint');
+var cache    = require('gulp-cached');
+
+gulp.task('browser-sync', function () {
+  browserSync.init({
+    server: {
+      baseDir: './'
+    }
+  });
+});
+
+gulp.task('scss-lint', function() {
+  gulp.src('./scss/**/*.scss')
+    .pipe(cache('scsslint'))
+    .pipe(scsslint());
+});
+
+gulp.task('serve', ['sass', 'browser-sync'], function () {
+  gulp.watch('./scss/**/*.scss', ['scss-lint', 'sass']);
+  gulp.watch('**/*.html').on('change', browserSync.reload);
+});
+
+gulp.task('sass', function () {
+  sass('scss/style.scss', { sourcemap: true, style: 'compact' })
+    .pipe(prefix('last 5 Chrome versions',
+                 'last 5 Firefox versions',
+                 'last 2 Safari versions',
+                 'ie >= 11',
+                 'iOS >= 7',
+                 'Android >= 4.3'))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('dist/css'))
+    .pipe(minifyCss())
+    .pipe(sourcemaps.write())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('dist/css'))
+    .pipe(browserSync.stream({ match: '**/*.css' }))
+  ;
+});
+
+gulp.task('default', ['sass', 'browser-sync'], function () {
+
+});
